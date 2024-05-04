feabd37073f56de5aff7890a53be54f2de3aaf40
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-05-04 16:35:20 +0700

    Day 5
    - Add adaptive for home part

--- src/components/mainSection/Mainsection.tsx
+++ src/components/mainSection/Mainsection.tsx
@@ -6,16 +6,23 @@ import SignUpButton from "./sigUpButton/SignUpButton";
 const Home = () => {
   return (
     <section className={styles.container}>
-      <div>
+      <div style={{ gridArea: "h1" }}>
         <h1 className={styles.h1}>Студия йоги и пилатеса</h1>
-
+      </div>
+      <div style={{ gridArea: "p" }}>
         <span className={styles.p}>
           Здесь мы поможем вам держать тело в тонусе, а душу - в гармонии.
         </span>
+      </div>
+      <div className={styles.btnContainer}>
         <SignUpButton />
+      </div>
+      <div style={{ gridArea: "adv" }}>
         <Advantages />
       </div>
-      <ImageWithSlider />
+      <div style={{ gridArea: "img", alignSelf: "center" }}>
+        <ImageWithSlider />
+      </div>
     </section>
   );
 };
