d5ed3925f109ee02a695ae5105e8f6347369549b
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-05-10 20:35:48 +0700

    Day 11

--- src/components/directionsInSub/topContent/TopContent.tsx
+++ src/components/directionsInSub/topContent/TopContent.tsx
@@ -1,10 +1,15 @@
+import Typography from "../../shared/typography/Typography";
 import styles from "./TopContent.module.scss";
 
 const TopContent = () => {
   return (
     <div className={styles.topContent}>
-      <h2 className={styles.h}>Все направления в одном абонементе</h2>
-      <p className={styles.p}>Не определились с направлением? Мы подскажем!</p>
+      <Typography variant="h2" className={styles.h}>
+        Все направления в одном абонементе
+      </Typography>
+      <Typography variant="h4" className={styles.p}>
+        Не определились с направлением? Мы подскажем!
+      </Typography>
     </div>
   );
 };
